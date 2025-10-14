151713c157ef698c2f16045fc124b464f939ae93
dhakal kiran <116267720+kirandhakal@users.noreply.github.com>
2025-10-14 15:35:07 +0545

    Update Home.jsx

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import "./Home.css"; 
-import NavBar from "./NavBar";
+// import NavBar from "./NavBar";
 import { useNavigate } from "react-router-dom";
 
 
