8caba7c9c86dc00815464722e0c5044d0b080f77
dhakalkiran <kirandhakal715@gmail.com>
2025-10-17 10:10:58 +0545

    fixremember me

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import "./App.css";
+import { useEffect } from "react";
 import {
   BrowserRouter as Router,
   Routes,
   Route,
   useNavigate,
 } from "react-router-dom";
+// import Main from "./Components/Main";
 import Home from "./Components/Home";
 import AllUser from "./Components/AllUser";
 import NavBar from "./Components/NavBar";
 import Login from "./Components/Login";
 import Signup from "./Components/Signup";
-import { useEffect } from "react";
+
 
 function App() {
   // Updated ProtectedRoute - now checks for admin token for /users route
@@ -30,10 +32,13 @@ function App() {
   return (
     <Router>
       <NavBar />
-
+{/* <Main /> */}
       <div className="App">
         <Routes>
           <Route path="/login" element={<Login />} />
+
+  
+
           <Route path="/signup" element={<Signup />} />
 
           <Route
